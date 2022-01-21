cff692ae8cbc7ed31e163454e2e6c4f89e99a4f7
ludosofo <jose-ramon.campos-munoz@capgemini.com>
2022-01-21 12:01:14 +0100

    Anadimos nueva funcion

--- src/main/java/com/capgemini/service/IProductoService.java
+++ src/main/java/com/capgemini/service/IProductoService.java
@@ -14,4 +14,8 @@ public interface IProductoService {
 	public Producto findById(long id);
 	public void delete(long id);
 	public Producto save(Producto producto);
+	
+	// Podríamos haberlo llamado findAll pero para no crear confusión lo llamos de forma diferente
+	// Recupera todos los productos sin paginar ni ordenar
+	public List<Producto> dameProductos();
 }
